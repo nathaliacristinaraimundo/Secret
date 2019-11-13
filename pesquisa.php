46d8d6d9d75998daf3eae54a532cbfe6eb9917be
aluno <aluno>
2019-11-13 13:10:28 -0200

    No progresso

---
+++ pesquisa.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Pesquisa de Usuário </title>
+	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+</head>
+<body>
+	<form class="max-w-sm p-8">
+  <input class="bg-gray-200 hover:bg-white hover:border-gray-300 focus:outline-none focus:bg-white focus:shadow-outline focus:border-gray-300 ">
+  <button class="bg-teal-500 hover:bg-teal-600 focus:outline-none focus:shadow-outline ">
+    Sign Up
+  </button>
+</form>
+	
+</body>
+</html>
