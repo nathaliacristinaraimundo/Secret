ee0048c5839e74a6ee15d3f42737167a18abba05
livia corlaite <liviacorlaite@hotmail.com>
2019-11-22 15:40:22 -0300

    começamos estilização

--- feed.php
+++ feed.php
@@ -4,32 +4,31 @@
 	<meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title>Feed - Secret</title>
-	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="iconmonstr-iconic-font-1.3.0/css/iconmonstr-iconic-font.min.css">
+  <link rel="stylesheet" type="text/css" href="estilo.css">
 
 </head>
 <body>
-	<div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
-  <div class="flex-shrink-0">
-    <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
-  </div>
-  <div class="ml-6 pt-1">
-    <h4 class="text-xl text-gray-900 leading-tight">ChitChat</h4>
-    <p class="text-base text-gray-600 leading-normal">You have a new message!</p>
-  </div>
-</div>
-<div class="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-  <div class="sm:flex sm:items-center px-6 py-4">
-    <img class="block mx-auto sm:mx-0 sm:flex-shrink-0 h-16 sm:h-24 rounded-full" src="Livia.jpg" alt="Woman's Face">
-    <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
-      <p class="text-xl leading-tight">Lívia Corlaite</p>
-      <p class="text-sm leading-tight text-gray-600">Uma louca que prende no porão a familia de ricardo machado rocha</p>
-      <div class="mt-4">
-        <button class="text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-xs font-semibold rounded-full px-4 py-1 leading-normal">Seguir</button>
-      </div>
-    </div>
-  </div>
-</div>
-<img class="w-16 md:w-32 lg:w-48 rounded-lg block mx-auto" src="livia2.jpg">
-	
+	<header>
+    <h1>Secret</h1> 
+  </header>
+  <main>
+     <div class="secret">
+       <div class="texto"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam architecto facilis quam doloribus adipisci voluptatem perferendis officia ratione dolorem, repudiandae, voluptate eos pariatur cumque, sequi ad provident aspernatur eius! Possimus.     
+       </div>
+        <div class="opcoes">
+          <div class="coracao"><span class="im im-heart"></span></div>
+          <div class="comentario"><span class="im im-twitch"></div>
+          
+        </div>
+     </div>
+  </main>
+  <nav>
+   <a href="#"><span class="im im-home"></span></a>
+   <a href="#"><span class="im im-magnifier"></span></a>
+   <a href="#"><span class="im im-plus"></span></a>   
+   <a href="#"><span class="im im-user-settings"></span></a>
+  </nav>
 </body>
 </html>
