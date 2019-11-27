48c56c3c99c835b27440b55c6d2e2031b2d637c8
aluno <aluno>
2019-11-27 12:24:12 -0300

    finalizando a tela de comentarios

--- feed.php
+++ feed.php
@@ -1,3 +1,29 @@
+<?php  
+  $feed=[ 
+    [
+    "texto"=> "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam architecto facilis quam doloribus adipisci voluptatem perferendis officia ratione dolorem, repudiandae, voluptate eos pariatur cumque, sequi ad provident aspernatur eius! Possimus.",
+    
+    "likes"=> "3",
+    
+    "comentario"=> "2"
+    ],
+
+    [
+     "texto"=> "A lógica do Lorem ninguém entende, portanto: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet recusandae maiores similique dicta natus libero exercitationem. Aliquam tempora ipsam optio est, reiciendis ratione omnis magni tenetur, facilis mollitia temporibus incidunt.",
+   
+    "likes"=> "56",
+   
+    "comentario"=> "22"
+    ]
+
+];
+
+
+
+
+?> 
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,15 +40,26 @@
     <h1>Secret</h1> 
   </header>
   <main>
+    <?php 
+    foreach ($feed as $secret):      
+    
+    ?>
      <div class="secret">
-       <div class="texto"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam architecto facilis quam doloribus adipisci voluptatem perferendis officia ratione dolorem, repudiandae, voluptate eos pariatur cumque, sequi ad provident aspernatur eius! Possimus.     
-       </div>
+       <div class="texto"> <?= $secret ["texto"] ?>       </div>
         <div class="opcoes">
-          <div class="coracao"><span class="im im-heart"></span></div>
-          <div class="comentario"><span class="im im-twitch"></div>
-          
+          <div class="coracao">
+            <span> <?= $secret ["likes"] ?> </span>
+            <span class="im im-heart"></span>            
+          </div>
+          <div class="comentario">
+            <span> <?= $secret ["comentario"] ?> </span>
+            <span class="im im-speech-bubble-comment"></span>            
+          </div>          
         </div>
      </div>
+     <?php 
+     endforeach;
+     ?>
   </main>
   <nav>
    <a href="#"><span class="im im-home"></span></a>
